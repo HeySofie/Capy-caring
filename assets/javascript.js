d65bf204be5c22bf596e81347b0d027271816418
HeySofie <sofie.feron1@gmail.com>
2023-02-03 09:42:33 +0100

    upload js and css

---
+++ assets/javascript.js
@@ -0,0 +1,57 @@
+var money = 0;// sets the start money in the begining
+var farm = document.getElementById('farm');
+farm.addEventListener('click', collect_food);
+var farm_grown = false;
+var food = false;
+
+
+//      the farm food grows every 10 seconds
+var growing_time = setInterval(grow_done, 10000);
+
+
+//      turns farm_frown to true after 10sec? passed- and is supposed to stop the timer
+function grow_done()
+{
+    farm_grown = true;
+    console.log(farm_grown);
+    clearInterval(growing_time);
+  
+}
+
+//      for when the food has grown and player clicks and stores the food
+function collect_food()
+{
+
+
+
+    if(farm_grown == true && food == false)
+    {
+        food = true;
+        farm_grown = false;
+        var growing_time = setInterval(grow_done, 10000);
+
+    }
+
+    else(farm_grown == false)
+    {
+
+    }
+
+    console.log('food collected?',food);
+
+}
+
+
+var plate = document.getElementById('plate');
+plate.addEventListener('click', put_food);
+var food_in_plate = false
+
+function put_food()
+{
+
+    if(food == true)
+    {
+
+    }
+
+}
