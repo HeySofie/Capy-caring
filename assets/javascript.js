a1f4a451fb9e46c634422d9391f4fc8384ba5b44
HeySofie <sofie.feron1@gmail.com>
2023-03-17 09:36:02 +0100

    cleaned up code

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,14 +1,14 @@
 var money = 0;// sets the start money in the begining
 var money_text = document.getElementById('money_number');
 var farm = document.getElementById('farm');
-farm.addEventListener('click', collect_food);
+farm.addEventListener('click' , collect_food);
 var farm_grown = false;
 var food = false;
 
 //      Model start
 var start_button = document.getElementById('start_button');
 var model = document.getElementById('model');
-start_button.addEventListener('click', start_game);
+start_button.addEventListener('click' , start_game);
 
 //funktion that runs once u press the button to start the game, and make the model not visible
 function start_game()
@@ -21,9 +21,9 @@ function start_game()
 //      Farm start
 function growing_time()//     funktion for the timeouts
 {
-    setTimeout(grow_done, 10000);
-    setTimeout(grow_33, 3300);
-    setTimeout(grow_66, 6600)
+    setTimeout( grow_done , 10000 );
+    setTimeout( grow_33 , 3300 );
+    setTimeout( grow_66 , 6600 )
 }
 
 //      after a time the farm changes to another img/growing time
@@ -40,7 +40,7 @@ function grow_66()
 function grow_done()
 {
     farm_grown = true;
-    console.log(farm_grown);
+    console.log( farm_grown );
     farm.src = "/assets/imgs/plant-finish.png"
 }
 
@@ -56,7 +56,7 @@ function collect_food()
     {
         food = true;
         farm_grown = false;
-        console.log('food collected?',food);
+        console.log( 'food collected?',food );
         farm.src ="/assets/imgs/plantV2many.png";
         growing_time();
 
@@ -64,7 +64,7 @@ function collect_food()
 
     else if(farm_grown == false)
     {
-        console.log('farm ready to be harvested?',farm_grown);
+        console.log( 'farm ready to be harvested?' , farm_grown );
     }
 
 }
@@ -74,13 +74,18 @@ function collect_food()
 //      Capybara start
 
 var Capybara = document.getElementById('capybara');
-Capybara.addEventListener('click', );
+Capybara.addEventListener('click',capy_interact );
 
 function capy_interact()
 {
-    Capybara
+    Capybara.src="/assets/imgs/capybara-sit.png";
+    setTimeout( 5000 , capychange_og );
 }
-
+ 
+function capychange_og()
+{
+    Capybara.src="assets/imgs/capybbara.png";
+}  
 //      Capybara end
 
 //      Plate start
@@ -91,12 +96,12 @@ var food_in_plate = false;
 function put_food() //abou putting the food in the plate
 {
 
-    if(food == true && food_in_plate == false)
+    if( food == true && food_in_plate == false )
     {
         plate.src="";
         food_in_plate = true;
         food = false;
-        setTimeout(food_eaten, 10000); //after 10 sec play food_eaten
+        setTimeout( food_eaten , 10000 ); //after 10 sec play food_eaten
     }
 
 }
@@ -105,7 +110,7 @@ function food_eaten()
 {
     plate.src="/assets/imgs/pixil-frame-0 (2).png";
     money++ ;
-    console.log(money);
+    console.log( money );
     money_text.innerHTML = money;
     
     
