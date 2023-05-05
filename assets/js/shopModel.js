6c3b30a1597f96ee3edc22368fb060fa2b986621
HeySofie <sofie.feron1@gmail.com>
2023-05-05 11:11:33 +0200

    updated the storing of hottub, look at comment

--- assets/js/shopModel.js
+++ assets/js/shopModel.js
@@ -5,10 +5,14 @@ var exit_button = document.getElementById('exit_button');
 var hottub_item = document.getElementById('hottub_item');
 var hottub = document.getElementById('hottub');
 var hottub_item_text = document.getElementById('hottub_item_text');
+var hottub_owned = false;
+//var stored_hottub = sessionStorage.getItem('hottub_owned');
+// create a objet where u define if the uppgrade is owned or not, and then save the object with a key "stored_objects" that u then 
 
 house.addEventListener('click', open_shop);
 exit_button.addEventListener('click', close_shop);
 hottub_item.addEventListener('click', buy_hottub);
+
 
 
 function open_shop()
@@ -33,7 +37,7 @@ function buy_hottub()
         hottub.style.visibility = 'visible';
         money-=5;
         money_text.innerHTML = money;
-        //owned_uppgrades.push("hottub");
+        hottub_owned = true;
         // sessionstorage("välj din egen nyckel", "namnet på item")
     }
 
