1ca717d80bec96389041eec58b8e0e2ed6b93256
HeySofie <sofie.feron1@gmail.com>
2023-05-08 11:05:42 +0200

    testing changing key value in object

--- assets/js/shopModel.js
+++ assets/js/shopModel.js
@@ -1,19 +1,22 @@
-var owned_uppgrades = [];
 var house = document.getElementById('house');
 var shop = document.getElementById('shop_model');
 var exit_button = document.getElementById('exit_button');
 var hottub_item = document.getElementById('hottub_item');
 var hottub = document.getElementById('hottub');
 var hottub_item_text = document.getElementById('hottub_item_text');
-var hottub_owned = false;
+
+var owned_uppgrades = {
+    hottub: false,
+
+};
+
 //var stored_hottub = sessionStorage.getItem('hottub_owned');
 // create a objet where u define if the uppgrade is owned or not, and then save the object with a key "stored_objects" that u then 
+//question, can i change a value inside a object?? if not the comment above wont work
 
 house.addEventListener('click', open_shop);
 exit_button.addEventListener('click', close_shop);
 hottub_item.addEventListener('click', buy_hottub);
-
-
 
 function open_shop()
 {
@@ -37,7 +40,7 @@ function buy_hottub()
         hottub.style.visibility = 'visible';
         money-=5;
         money_text.innerHTML = money;
-        hottub_owned = true;
+        owned_uppgrades['hottub'] = true;
         // sessionstorage("välj din egen nyckel", "namnet på item")
     }
 
