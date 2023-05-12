0fb83b7efde28b136c55328717f8e368833885e9
HeySofie <sofie.feron1@gmail.com>
2023-05-12 10:27:51 +0200

    trying to save hottub to sesstion-check comment

--- assets/js/shopModel.js
+++ assets/js/shopModel.js
@@ -4,20 +4,21 @@ var exit_button = document.getElementById('exit_button');
 var hottub_item = document.getElementById('hottub_item');
 var hottub = document.getElementById('hottub');
 var hottub_item_text = document.getElementById('hottub_item_text');
+var hottub_owned = false;
+var stored_hottub = sessionStorage.getitem('hottub'); // this is breaking the thing
+house.addEventListener('click', open_shop);
+exit_button.addEventListener('click', close_shop);
+hottub_item.addEventListener('click', buy_hottub);
 
-var owned_uppgrades = {
-    hottub: false,
-};
 
-console.log( "is hottub owned?" + owned_uppgrades['hottub'] );
+if(hottub_owned == true)
+{
+    hottub.style.visibility = 'visible';
+}
 
 //var stored_hottub = sessionStorage.getItem('hottub_owned');
 // create a objet where u define if the uppgrade is owned or not, and then save the object with a key "stored_objects" that u then 
 //question, can i change a value inside a object?? if not then the comment above wont work
-
-house.addEventListener('click', open_shop);
-exit_button.addEventListener('click', close_shop);
-hottub_item.addEventListener('click', buy_hottub);
 
 function open_shop()
 {
@@ -31,19 +32,16 @@ function close_shop()
 
 function buy_hottub()
 {
-    if(parseFloat(money) < 5 || owned_uppgrades["hottub"] == true)
+    if(parseFloat(money) < 1 || owned_uppgrades["hottub"] == true)
     {
         hottub_item_text.innerHTML = "cant buy";
     }
 
     else
     {
-        hottub.style.visibility = 'visible';
-        money-=5;
+        money-=1;
         money_text.innerHTML = money;
-        owned_uppgrades['hottub'] = true;
-        console.log( owned_uppgrades['hottub'] +' hottub owned!');
-        // sessionstorage("välj din egen nyckel", "namnet på item")
+        hottub_owned = true;
     }
 
 }
